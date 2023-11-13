f97fb89c919c8693609a07a89806c1316d73549d
Ratmir Gaitov <gaitovrat@gmail.com>
2023-11-13 19:04:07 +0100

    Implement FXOS8700CQ

--- source/sensors/FXOS8700CQ.cpp
+++ source/sensors/FXOS8700CQ.cpp
@@ -7,12 +7,26 @@
 
 #include <sensors/FXOS8700CQ.h>
 
-FXOS8700CQ::FXOS8700CQ() {
-	// TODO Auto-generated constructor stub
+#include <pin_mux.h>
 
+static constexpr uint8_t sWhoAmI = 0x0DU;
+
+FXOS8700CQ::FXOS8700CQ() : ASensor(I2C0),
+		mAddresses({0x1CU, 0x1DU, 0x1EU, 0x1FU})
+{}
+
+void FXOS8700CQ::Init()
+{
+	BOARD_InitACCEL_I2CPins();
+	FindSlaveAddress();
 }
 
-FXOS8700CQ::~FXOS8700CQ() {
-	// TODO Auto-generated destructor stub
+uint8_t FXOS8700CQ::WhoAmIRegister() const
+{
+	return sWhoAmI;
 }
 
+const std::vector<uint8_t>& FXOS8700CQ::SlaveAddresses() const
+{
+	return mAddresses;
+}
--- source/sensors/FXOS8700CQ.h
+++ source/sensors/FXOS8700CQ.h
@@ -8,11 +8,27 @@
 #ifndef SENSORS_FXOS8700CQ_H_
 #define SENSORS_FXOS8700CQ_H_
 
-class FXOS8700CQ {
+#include "sensors/ASensor.h"
+
+#include <vector>
+#include <cstdint>
+
+class FXOS8700CQ : public ASensor
+{
 public:
+
 	FXOS8700CQ();
 
-	virtual ~FXOS8700CQ();
+	virtual ~FXOS8700CQ() = default;
+
+	void Init();
+
+	uint8_t WhoAmIRegister() const override;
+
+	const std::vector<uint8_t>& SlaveAddresses() const override;
+
+private:
+	std::vector<uint8_t> mAddresses;
 };
 
 #endif /* SENSORS_FXOS8700CQ_H_ */
