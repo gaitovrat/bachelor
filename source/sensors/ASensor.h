16751cb1bb0e5bb8ad5cd063205b8f611735d613
Ratmir Gaitov <gaitovrat@gmail.com>
2023-11-18 15:02:29 +0100

    Implement FOS8700CQ sensor

--- source/sensors/ASensor.h
+++ source/sensors/ASensor.h
@@ -21,21 +21,14 @@ public:
 
 	virtual void Init();
 
-	void FindDeviceAddress();
-
-	uint8_t DeviceAddress() const;
-
-	virtual uint8_t WhoAmIRegister() const = 0;
-
-	virtual const std::vector<uint8_t>& DeviceAddresses() const = 0;
+	virtual uint8_t DeviceAddress() const = 0;
 
 	status_t ReadRegister(uint8_t registerAddress, uint8_t *pBuffer, size_t bufferSize) const;
 
 	status_t WriteRegister(uint8_t registerAddress, uint8_t *pBuffer, size_t bufferSize);
 
 private:
-	I2C_Type *mpBase;
-	uint8_t mDeviceAddress;
+	I2C_Type *mp_Base;
 };
 
 #endif /* SENSORS_ASENSOR_H_ */
