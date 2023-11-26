d8783341172812d525576a8c78fff2d8fa785e05
Ratmir Gaitov <gaitovrat@gmail.com>
2023-11-26 11:43:16 +0100

    Implement SerialClient

--- carui/serialclient.cpp
+++ carui/serialclient.cpp
@@ -1,3 +1,19 @@
 #include "serialclient.h"
 
-SerialClient::SerialClient() {}
+#include <QDebug>
+
+static constexpr int BUFFER_SIZE = 1024;
+
+SerialClient::SerialClient(const QSerialPort::BaudRate baudRate, QObject *parent) :
+    port(parent),
+    BaseClient(&this->port, &QSerialPort::readyRead, parent) {
+    port.setBaudRate(baudRate);
+}
+
+SerialClient::SerialClient(QObject *parent) : SerialClient(QSerialPort::Baud115200, parent) {}
+
+void SerialClient::emitData() {
+    const QByteArray &buffer = this->port.readAll();
+
+    qDebug() << buffer.size() << ' ' << buffer.data();
+}
