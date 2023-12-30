4e81de373f9ed52cac8cabb9fc6bc1808f29b36b
Ratmir Gaitov <gaitovrat@gmail.com>
2023-12-30 15:34:29 +0100

    Link qt and carui

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,10 +1,7 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    std::cout << argc << std::endl;
+
+    return 0;
 }
