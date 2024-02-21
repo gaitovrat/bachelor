9b62094d343f6ef2c2567e204a1144f0f2c75cd1
Ratmir Gaitov <gaitovrat@gmail.com>
2024-02-21 14:05:04 +0100

    Fix includes

--- CarQt/Src/Window/MainWindow.cpp
+++ CarQt/Src/Window/MainWindow.cpp
@@ -5,10 +5,10 @@
 
 #include "./ui_MainWindow.h"
 #include "Settings.h"
-#include "client/BaseClient.h"
-#include "client/SerialClient.h"
-#include "client/UdpClient.h"
-#include "window/SettingsWindow.h"
+#include "Client/BaseClient.h"
+#include "Client/SerialClient.h"
+#include "Client/UdpClient.h"
+#include "Window/SettingsWindow.h"
 
 MainWindow::MainWindow(const QString& name, QWidget* parent)
     : QMainWindow(parent),
