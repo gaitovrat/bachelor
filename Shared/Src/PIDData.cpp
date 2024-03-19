d19ef6a797fbdf29537602aaabcb82a888876742
Ratmir Gaitov <gaitovrat@gmail.com>
2024-03-19 20:59:52 +0100

    Add MCU cmake files

--- Shared/Src/PIDData.cpp
+++ Shared/Src/PIDData.cpp
@@ -2,6 +2,7 @@
 
 using namespace Shared;
 
-PIDData::PIDData(double p, double i, double d) : P(p), I(i), D(d), Input(0), SetPoint(0), Output(0) {}
+PIDData::PIDData(double p, double i, double d)
+    : P(p), I(i), D(d), Input(0), Output(0), SetPoint(0) {}
 
 PIDData::PIDData() : PIDData(0, 0, 0) {}
